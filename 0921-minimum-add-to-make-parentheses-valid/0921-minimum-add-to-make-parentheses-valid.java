e923cff56470113678c58b4accd3e7699255a827
Aksh Usrathe <akshusrathe007@gmail.com>
2026-03-20 17:04:42 +0530

    Time: 0 ms (100%), Space: 42.5 MB (98.21%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -1,19 +1,36 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        Stack<Character>stack = new Stack<>();
+        // Stack<Character>stack = new Stack<>();
 
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(ch == '(')
-            stack.push(ch);
-            else {
-                if(!stack.isEmpty() && stack.peek()=='(')
-                stack.pop();
-                else 
-                stack.push(ch);
-            }
+        // for(int i=0;i<s.length();i++){
+        //     char ch = s.charAt(i);
+        //     if(ch == '(')
+        //     stack.push(ch);
+        //     else {
+        //         if(!stack.isEmpty() && stack.peek()=='(')
+        //         stack.pop();
+        //         else 
+        //         stack.push(ch);
+        //     }
             
+        // }
+        // return stack.size();
+
+
+        int open=0;
+        int close = 0;
+        for(char ch: s.toCharArray()){
+            if(ch == '(')
+            open++;
+            else{
+                if(open<=0)
+                close++;
+                else 
+                open--;
+            }
         }
-        return stack.size();
+        return open+close;
     }
+
+
 }
